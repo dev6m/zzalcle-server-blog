d66a7fe68dcb932c717e539691442f253965d397
sungsikyang92 <sungsik.yang92@gmail.com>
2022-03-12 16:41:15 +0900

    init

---
+++ src/main/java/com/dev6m/zzalcleblog/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.dev6m.zzalcleblog;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ZzalcleblogApplication.class);
+	}
+
+}
---
+++ src/main/java/com/dev6m/zzalcleblog/ZzalcleblogApplication.java
@@ -0,0 +1,13 @@
+package com.dev6m.zzalcleblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzalcleblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzalcleblogApplication.class, args);
+	}
+
+}
